8c7ed9889f21c42e7f528bf0791c491f260c3816
Adam Brown <audrummer15@gmail.com>
2018-02-06 09:52:49 -0500

    Update socat in screen.

--- nysm-setup.sh
+++ nysm-setup.sh
@@ -50,7 +50,8 @@ create_socat_instance() {
   read -r -p "What IP/hostname should socat forward $2data to? " outbound_host
   read -r -p "What port should socat forward $2data to? " outbound_port
 
-  screen -A -m -d -S "socat-$inbound_port" "socat -d -d -lf /var/log/socat.log TCP-LISTEN:$inbound_port,fork,range=$RANGE TCP-CONNECT:$outbound_host:$outbound_port"
+  screen -mdS "socat-$outbound_host-$outbound_port"
+  screen -S "socat-$outbound_host-$outbound_port" -p 0 -X stuff 'socat -d -d -lf /var/log/socat.log TCP-LISTEN:$inbound_port,fork,range=$RANGE TCP-CONNECT:$outbound_host:$outbound_port\n'
 
   ps aux | grep socat | grep $inbound_port
   netstat -tlpn | grep socat | grep $inbound_port
