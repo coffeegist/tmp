1e07e6c2d78dc3d8f02b2b671fc0c0f47a023663
Adam Brown <audrummer15@gmail.com>
2018-02-06 10:55:58 -0500

    dumb

--- nysm-setup.sh
+++ nysm-setup.sh
@@ -50,7 +50,7 @@ create_socat_instance() {
   read -r -p "What IP/hostname should socat forward $2data to? " outbound_host
   read -r -p "What port should socat forward $2data to? " outbound_port
 
-  screen -mdS "socat-$outbound_port-$outbound_host" && screen -S "socat-$outbound_host-$outbound_port" -p 0 -X stuff "\"socat -d -d -lf /var/log/socat.log TCP-LISTEN:$inbound_port,fork,range=$RANGE TCP-CONNECT:$outbound_host:$outbound_port\"$'\n'"
+  screen -mdS "socat-$outbound_port-$outbound_host" && screen -S "socat-$outbound_port-$outbound_host" -p 0 -X stuff "\"socat -d -d -lf /var/log/socat.log TCP-LISTEN:$inbound_port,fork,range=$RANGE TCP-CONNECT:$outbound_host:$outbound_port\"$'\n'"
 
   ps aux | grep socat | grep $inbound_port
   netstat -tlpn | grep socat | grep $inbound_port
